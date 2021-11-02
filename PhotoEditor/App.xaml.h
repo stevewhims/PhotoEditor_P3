41055c84f8771b4985ef14bb0a191ca34a9e63ff
Steve White <31261191+stevewhims@users.noreply.github.com>
2021-11-02 12:46:45 -0700

    Add project files.

---
+++ PhotoEditor/App.xaml.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::PhotoEditor::implementation
+{
+    struct App : AppT<App>
+    {
+        App();
+        Microsoft::UI::Xaml::Controls::Frame CreateRootFrame();
+
+        void OnLaunched(Microsoft::UI::Xaml::LaunchActivatedEventArgs const&);
+        void OnNavigationFailed(IInspectable const&, Microsoft::UI::Xaml::Navigation::NavigationFailedEventArgs const&);
+
+        static winrt::Microsoft::UI::Xaml::Window Window() { return window; };
+
+    private:
+        static winrt::Microsoft::UI::Xaml::Window window;
+    };
+}
---
+++ PhotoEditor/MainWindow.xaml.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::PhotoEditor::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+    };
+}
+
+namespace winrt::PhotoEditor::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
